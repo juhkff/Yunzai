ab84faf904c8f59663274ab0d10cbe6c840fea16
juhkff <363257597@qq.com>
2025-01-11 16:40:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# TRSS-Yunzai
+
+自用群 BOT
+
+# 配置
+
+```console
+nvm use 23.3.0
+cd Yunzai
+pnpm i
+```
---
+++ app.js
@@ -0,0 +1,22 @@
+switch (process.env.app_type || process.argv[2]) {
+  case "pm2":
+  case "start": {
+    global.Bot = new (await import("./lib/bot.js")).default
+    Bot.run()
+    break
+  } case "stop": {
+    const cfg = (await import("./lib/config/config.js")).default
+    const fetch = (await import("node-fetch")).default
+    try {
+      await fetch(`http://localhost:${cfg.server.port}/exit`, { headers: cfg.server.auth || undefined })
+    } catch {}
+    process.exit()
+  } default: {
+    const { spawnSync } = await import("node:child_process")
+    while (!spawnSync(process.argv[0],
+      [process.argv[1], "start"],
+      { stdio: "inherit" },
+    ).status) {}
+    process.exit()
+  }
+}
