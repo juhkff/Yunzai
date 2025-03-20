176970c830290fa13c6a5f8608988dd366ec86a9
juhkff <363257597@qq.com>
2025-03-21 05:15:19 +0800

    feat(autoReply): 支持火山方舟 API
    - 新增 ArkEngine 类实现火山方舟 API 调用
    - 在 chatMap 中添加火山方舟支持
    - 更新配置界面，增加模型请求 URL 设置
    - 优化配置更新逻辑，支持动态加载模型列表

--- plugins/juhkff-plugin/model/chat_api.js
+++ plugins/juhkff-plugin/model/chat_api.js
@@ -4,6 +4,8 @@ import setting from "#juhkff.setting";
 import { EMOTION_KEY } from "#juhkff.redis";
 import Objects from "#juhkff.kits";
 
+/************************** 接口和基础类定义 **************************/
+
 export const ChatInterface = {
   generateRequest: Symbol("generateRequest"),
   getModelMap: Symbol("getModelMap"),
@@ -26,17 +28,20 @@ ChatInterface.generateRequest = async function ({
   image_list = {},
   image_type = false,
   useSystemRole = true,
-}) {};
+}) { };
 
-ChatInterface.getModelMap = function () {};
+ChatInterface.getModelMap = function () { };
 
 class ChatApi {
   constructor() {
     this.Config = setting.getConfig("autoReply");
     this.ModelMap = {};
+    this.ApiBaseUrl = undefined;
+    // 默认情况下模型等信息在指定API后提供，禁止用户自行输入
+    this.shouldInputSelf = false;
   }
 
-  [ChatInterface.getModelMap]() {}
+  [ChatInterface.getModelMap]() { }
 
   async [ChatInterface.generateRequest]({
     apiKey,
@@ -46,8 +51,11 @@ class ChatApi {
     image_list = {},
     image_type = false,
     useSystemRole = true,
-  }) {}
+  }) { }
 }
+
+
+/************************** 各API实现 **************************/
 
 export class Siliconflow extends ChatApi {
   constructor() {
@@ -193,7 +201,6 @@ export class DeepSeek extends ChatApi {
   }
 
   [ChatInterface.getModelMap]() {
-    this.ModelMap = {};
     this.ModelMap = {
       "deepseek-chat": this.deepseek_chat.bind(this),
       "deepseek-reasoner": this.deepseek_reasoner.bind(this),
@@ -390,6 +397,127 @@ export class DeepSeek extends ChatApi {
   }
 }
 
+export class ArkEngine extends ChatApi {
+  constructor(Config) {
+    super(Config);
+    // 需要用户自行输入模型等信息
+    this.shouldInputSelf = true;
+  }
+
+  // 自定义模型，不依靠成员变量
+  [ChatInterface.getModelMap]() { }
+
+  async [ChatInterface.generateRequest]({
+    apiKey,
+    model,
+    input,
+    historyMessages = [],
+    image_list = {},
+    image_type = false,
+    useSystemRole = true,
+  }) {
+    // 构造请求体
+    var request = {
+      url: `${this.Config.apiCustomUrl}`,
+      options: {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+        },
+        body: {
+          model: model,
+          messages: [],
+          stream: false,
+          temperature: 1.5,
+        },
+      },
+    };
+
+    // var response = await this.ModelMap[model](
+    var response = await this.commonRequest(
+      request,
+      input,
+      historyMessages,
+      image_list,
+      image_type,
+      useSystemRole
+    );
+    // 调用返回结果的头尾容易有换行符，进行处理
+    response = response.replace(/^\n+|\n+$/g, "");
+    return response;
+  }
+  async commonRequest(
+    request,
+    input,
+    historyMessages = [],
+    image_list = {},
+    image_type = false,
+    useSystemRole
+  ) {
+    if (useSystemRole) {
+      var systemContent = await generateSystemContent(
+        this.Config.useEmotion,
+        this.Config.chatPrompt
+      );
+      request.options.body.messages.push(systemContent);
+    }
+
+    // 添加历史对话
+    if (historyMessages && historyMessages.length > 0) {
+      historyMessages.forEach((msg) => {
+        // 不是图片时添加
+        if (!msg.imageBase64) {
+          request.options.body.messages.push({
+            role: msg.role,
+            content: msg.content,
+          });
+        }
+      });
+    }
+    request.options.body.messages.push({
+      role: "user",
+      content: input,
+    });
+
+    logger.mark(
+      `\n[autoReply]火山方舟 API调用，请求内容：${JSON.stringify(
+        request,
+        null,
+        2
+      )}`
+    );
+
+    var response;
+    try {
+      request.options.body = JSON.stringify(request.options.body);
+      response = await fetch(request.url, request.options);
+
+      const data = await response.json();
+
+      if (data?.choices?.[0]?.message?.content) {
+        return data.choices[0].message.content;
+      } else {
+        logger.error(
+          "[autoReply]火山方舟调用失败：",
+          JSON.stringify(data, null, 2)
+        );
+        return "[autoReply]火山方舟调用失败，详情请查阅控制台。";
+      }
+    } catch (error) {
+      logger.error(
+        `[autoReply]火山方舟调用失败, 请求返回结果：${JSON.stringify(
+          response
+        )}\n`,
+        error
+      );
+      return "[autoReply]火山方舟调用失败，详情请查阅控制台。";
+    }
+  }
+}
+
+/************************** 函数调用 **************************/
+
 /**
  * 生成 role = system 的内容
  * @param {*} useEmotion 是否使用情感
@@ -406,13 +534,19 @@ async function generateSystemContent(useEmotion, chatPrompt) {
     // todo 按deepseek-r1的模板修正格式，之后有问题再说
     content: useEmotion
       ? `${chatPrompt} \n 你的情感倾向——${emotionPrompt
-          .replace(/\n/g, "")
-          .replace(/\s+/g, "")}`
+        .replace(/\n/g, "")
+        .replace(/\s+/g, "")}`
       : chatPrompt,
   };
 }
 
+/************************** 模型列表 **************************/
+
+/**
+ * 聊天模型列表，新增的都加里面
+ */
 export const chatMap = {
   siliconflow: new Siliconflow(),
   deepseek: new DeepSeek(),
+  "火山方舟": new ArkEngine(),
 };
