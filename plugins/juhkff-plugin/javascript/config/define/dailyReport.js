958e47224f117822cf9f032665b5db5d992ca1c7
juhkff <363257597@qq.com>
2025-04-28 18:58:20 +0800

    sync

--- plugins/juhkff-plugin/javascript/config/define/dailyReport.js
+++
@@ -1,9 +0,0 @@
-const dailyReport = {
-    useDailyReport: true,
-    alapiToken: "",
-    dailyReportFullShow: true,
-    push: true,
-    dailyReportTime: "0 0 12 * * ?"
-};
-export default dailyReport;
-//# sourceMappingURL=dailyReport.js.map
--- plugins/juhkff-plugin/src/import.d.ts
+++
@@ -1,7 +0,0 @@
-/*-------------------------------- 声明全局变量 --------------------------------*/
-declare const Bot: any;
-declare const redis: any;
-declare const logger: any;
-declare const plugin: any;
-declare const Renderer: any;
-declare const segment: any;
